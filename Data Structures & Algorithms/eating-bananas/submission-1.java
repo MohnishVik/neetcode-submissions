0db07c422b0c8c05892ab0dac6f09d5da78e1972
Mohnish S <154992781+MohnishVik@users.noreply.github.com>
2026-07-13 00:05:37 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) 
+    {
+        int left=1;
+        int right=1;
+        for(int pile:piles)
+        {
+            if(pile>right)
+            {
+                right=pile;
+            }
+        }
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;//speed
+            int hours=0;
+            for(int pile:piles)
+            {
+                hours+=(pile+mid-1)/mid;
+            }
+            if(hours<=h)
+            {
+                right=mid-1;
+            }
+            else
+            {
+                left=mid+1;
+            }
+        }
+        return left;    
+    }
+}
